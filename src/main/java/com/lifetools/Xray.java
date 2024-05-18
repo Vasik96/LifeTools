03172fbe5dec11794cd8fa1b6f88d76b2f9169a9
vasik96 <leman.vaclav@gmail.com>
2024-05-18 16:15:57 +0200

    Added partial Xray

--- src/main/java/com/lifetools/Xray.java
+++ src/main/java/com/lifetools/Xray.java
@@ -30,7 +30,22 @@ public class Xray implements ClientModInitializer {
         visibleBlocks.add(Blocks.GOLD_ORE);
         visibleBlocks.add(Blocks.IRON_ORE);
         visibleBlocks.add(Blocks.COAL_ORE);
-        visibleBlocks.add(Blocks.EMERALD_ORE);
+        visibleBlocks.add(Blocks.COPPER_ORE);
+        visibleBlocks.add(Blocks.REDSTONE_ORE);
+        visibleBlocks.add(Blocks.LAPIS_ORE);
+        visibleBlocks.add(Blocks.DEEPSLATE_EMERALD_ORE);
+        visibleBlocks.add(Blocks.DEEPSLATE_COAL_ORE);
+        visibleBlocks.add(Blocks.DEEPSLATE_COPPER_ORE);
+        visibleBlocks.add(Blocks.DEEPSLATE_GOLD_ORE);
+        visibleBlocks.add(Blocks.DEEPSLATE_DIAMOND_ORE);
+        visibleBlocks.add(Blocks.DEEPSLATE_LAPIS_ORE);
+        visibleBlocks.add(Blocks.DEEPSLATE_REDSTONE_ORE);
+        visibleBlocks.add(Blocks.DEEPSLATE_IRON_ORE);
+        visibleBlocks.add(Blocks.ANCIENT_DEBRIS);
+        visibleBlocks.add(Blocks.NETHER_GOLD_ORE);
+        visibleBlocks.add(Blocks.RAW_IRON_BLOCK);
+        visibleBlocks.add(Blocks.RAW_COPPER_BLOCK);
+        visibleBlocks.add(Blocks.GILDED_BLACKSTONE);
 
         // Register key binding
         keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
