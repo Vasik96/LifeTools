a26a201c83eb2fcb709a7155decec349faf680a3
Vasik96 <leman.vaclav@gmail.com>
2024-08-31 14:23:37 +0200

    Improved Xray

---
+++ src/main/java/com/lifetools/XrayList.java
@@ -0,0 +1,78 @@
+package com.lifetools;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class XrayList {
+
+    private static final Set<Block> XRAY_BLOCKS = new HashSet<>();
+
+    static {
+        XRAY_BLOCKS.add(Blocks.DIAMOND_ORE);
+        XRAY_BLOCKS.add(Blocks.GOLD_ORE);
+        XRAY_BLOCKS.add(Blocks.IRON_ORE);
+        XRAY_BLOCKS.add(Blocks.COAL_ORE);
+        XRAY_BLOCKS.add(Blocks.COPPER_ORE);
+        XRAY_BLOCKS.add(Blocks.REDSTONE_ORE);
+        XRAY_BLOCKS.add(Blocks.LAPIS_ORE);
+        XRAY_BLOCKS.add(Blocks.EMERALD_ORE);
+        XRAY_BLOCKS.add(Blocks.DEEPSLATE_DIAMOND_ORE);
+        XRAY_BLOCKS.add(Blocks.DEEPSLATE_GOLD_ORE);
+        XRAY_BLOCKS.add(Blocks.DEEPSLATE_IRON_ORE);
+        XRAY_BLOCKS.add(Blocks.DEEPSLATE_COAL_ORE);
+        XRAY_BLOCKS.add(Blocks.DEEPSLATE_COPPER_ORE);
+        XRAY_BLOCKS.add(Blocks.DEEPSLATE_REDSTONE_ORE);
+        XRAY_BLOCKS.add(Blocks.DEEPSLATE_LAPIS_ORE);
+        XRAY_BLOCKS.add(Blocks.DEEPSLATE_EMERALD_ORE);
+        XRAY_BLOCKS.add(Blocks.LAVA);
+        XRAY_BLOCKS.add(Blocks.SPAWNER);
+        XRAY_BLOCKS.add(Blocks.TRIAL_SPAWNER);
+        XRAY_BLOCKS.add(Blocks.END_PORTAL_FRAME);
+        XRAY_BLOCKS.add(Blocks.END_PORTAL);
+        XRAY_BLOCKS.add(Blocks.END_GATEWAY);
+        XRAY_BLOCKS.add(Blocks.NETHER_PORTAL);
+        XRAY_BLOCKS.add(Blocks.BEDROCK);
+        XRAY_BLOCKS.add(Blocks.BARREL);
+        XRAY_BLOCKS.add(Blocks.CHEST);
+        XRAY_BLOCKS.add(Blocks.TRAPPED_CHEST);
+        XRAY_BLOCKS.add(Blocks.ENDER_CHEST);
+        XRAY_BLOCKS.add(Blocks.NETHER_QUARTZ_ORE);
+        XRAY_BLOCKS.add(Blocks.NETHER_GOLD_ORE);
+        XRAY_BLOCKS.add(Blocks.GILDED_BLACKSTONE);
+        XRAY_BLOCKS.add(Blocks.WHITE_SHULKER_BOX);
+        XRAY_BLOCKS.add(Blocks.ORANGE_SHULKER_BOX);
+        XRAY_BLOCKS.add(Blocks.MAGENTA_SHULKER_BOX);
+        XRAY_BLOCKS.add(Blocks.LIGHT_BLUE_SHULKER_BOX);
+        XRAY_BLOCKS.add(Blocks.YELLOW_SHULKER_BOX);
+        XRAY_BLOCKS.add(Blocks.LIME_SHULKER_BOX);
+        XRAY_BLOCKS.add(Blocks.PINK_SHULKER_BOX);
+        XRAY_BLOCKS.add(Blocks.GRAY_SHULKER_BOX);
+        XRAY_BLOCKS.add(Blocks.LIGHT_GRAY_SHULKER_BOX);
+        XRAY_BLOCKS.add(Blocks.CYAN_SHULKER_BOX);
+        XRAY_BLOCKS.add(Blocks.PURPLE_SHULKER_BOX);
+        XRAY_BLOCKS.add(Blocks.BLUE_SHULKER_BOX);
+        XRAY_BLOCKS.add(Blocks.BROWN_SHULKER_BOX);
+        XRAY_BLOCKS.add(Blocks.GREEN_SHULKER_BOX);
+        XRAY_BLOCKS.add(Blocks.RED_SHULKER_BOX);
+        XRAY_BLOCKS.add(Blocks.BLACK_SHULKER_BOX);
+        XRAY_BLOCKS.add(Blocks.SHULKER_BOX);
+        XRAY_BLOCKS.add(Blocks.ANCIENT_DEBRIS);
+
+        XRAY_BLOCKS.add(Blocks.COAL_BLOCK);
+        XRAY_BLOCKS.add(Blocks.COPPER_BLOCK);
+        XRAY_BLOCKS.add(Blocks.IRON_BLOCK);
+        XRAY_BLOCKS.add(Blocks.GOLD_BLOCK);
+        XRAY_BLOCKS.add(Blocks.DIAMOND_BLOCK);
+        XRAY_BLOCKS.add(Blocks.EMERALD_BLOCK);
+        XRAY_BLOCKS.add(Blocks.NETHERITE_BLOCK);
+        XRAY_BLOCKS.add(Blocks.LAPIS_BLOCK);
+        XRAY_BLOCKS.add(Blocks.REDSTONE_BLOCK);
+    }
+
+    public static boolean isXrayBlock(Block block) {
+        return XRAY_BLOCKS.contains(block);
+    }
+}
--- src/main/java/com/lifetools/mixins/MixinBlockOcclusionCache.java
+++ src/main/java/com/lifetools/mixins/MixinBlockOcclusionCache.java
@@ -1,14 +1,13 @@
 package com.lifetools.mixins;
 
 import com.lifetools.XrayConfig;
+import com.lifetools.XrayList;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -17,11 +16,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(targets = "me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockOcclusionCache")
 public class MixinBlockOcclusionCache {
 
-    @Inject(at = @At("HEAD"), method = "shouldDrawSide", cancellable = true) //shouldDrawSide will not get resolved here as this mixin is only working with sodium
-    // and is used for sodium compatibility
+    @Inject(at = @At("HEAD"), method = "shouldDrawSide", cancellable = true)
     private void shouldDrawSide(BlockState state, BlockView reader, BlockPos pos, Direction face, CallbackInfoReturnable<Boolean> ci) {
         if (XrayConfig.ENABLED) {
-            if (isXrayBlock(state)) {
+            if (XrayList.isXrayBlock(state.getBlock())) {
                 // Force the side to be rendered if it's an X-ray block
                 ci.setReturnValue(true);
                 ci.cancel();
@@ -31,59 +29,5 @@ public class MixinBlockOcclusionCache {
                 ci.cancel();
             }
         }
-    }
-
-    @Unique
-    private boolean isXrayBlock(BlockState state) {
-        return state.getBlock() == Blocks.DIAMOND_ORE ||
-                state.getBlock() == Blocks.GOLD_ORE ||
-                state.getBlock() == Blocks.IRON_ORE ||
-                state.getBlock() == Blocks.COAL_ORE ||
-                state.getBlock() == Blocks.COPPER_ORE ||
-                state.getBlock() == Blocks.REDSTONE_ORE ||
-                state.getBlock() == Blocks.LAPIS_ORE ||
-                state.getBlock() == Blocks.EMERALD_ORE ||
-                state.getBlock() == Blocks.DEEPSLATE_DIAMOND_ORE ||
-                state.getBlock() == Blocks.DEEPSLATE_GOLD_ORE ||
-                state.getBlock() == Blocks.DEEPSLATE_IRON_ORE ||
-                state.getBlock() == Blocks.DEEPSLATE_COAL_ORE ||
-                state.getBlock() == Blocks.DEEPSLATE_COPPER_ORE ||
-                state.getBlock() == Blocks.DEEPSLATE_REDSTONE_ORE ||
-                state.getBlock() == Blocks.DEEPSLATE_LAPIS_ORE ||
-                state.getBlock() == Blocks.DEEPSLATE_EMERALD_ORE ||
-                state.getBlock() == Blocks.LAVA ||
-                state.getBlock() == Blocks.SPAWNER ||
-                state.getBlock() == Blocks.TRIAL_SPAWNER ||
-                state.getBlock() == Blocks.END_PORTAL_FRAME ||
-                state.getBlock() == Blocks.END_PORTAL ||
-                state.getBlock() == Blocks.NETHER_PORTAL ||
-                state.getBlock() == Blocks.BEDROCK ||
-                state.getBlock() == Blocks.BARREL ||
-                state.getBlock() == Blocks.CHEST ||
-                state.getBlock() == Blocks.TRAPPED_CHEST ||
-                state.getBlock() == Blocks.NETHER_QUARTZ_ORE ||
-                state.getBlock() == Blocks.NETHER_GOLD_ORE ||
-                state.getBlock() == Blocks.GILDED_BLACKSTONE||
-
-
-                state.getBlock() == Blocks.WHITE_SHULKER_BOX ||
-                state.getBlock() == Blocks.ORANGE_SHULKER_BOX ||
-                state.getBlock() == Blocks.MAGENTA_SHULKER_BOX ||
-                state.getBlock() == Blocks.LIGHT_BLUE_SHULKER_BOX ||
-                state.getBlock() == Blocks.YELLOW_SHULKER_BOX ||
-                state.getBlock() == Blocks.LIME_SHULKER_BOX ||
-                state.getBlock() == Blocks.PINK_SHULKER_BOX ||
-                state.getBlock() == Blocks.GRAY_SHULKER_BOX ||
-                state.getBlock() == Blocks.LIGHT_GRAY_SHULKER_BOX ||
-                state.getBlock() == Blocks.CYAN_SHULKER_BOX ||
-                state.getBlock() == Blocks.PURPLE_SHULKER_BOX ||
-                state.getBlock() == Blocks.BLUE_SHULKER_BOX ||
-                state.getBlock() == Blocks.BROWN_SHULKER_BOX ||
-                state.getBlock() == Blocks.GREEN_SHULKER_BOX ||
-                state.getBlock() == Blocks.RED_SHULKER_BOX ||
-                state.getBlock() == Blocks.BLACK_SHULKER_BOX ||
-                state.getBlock() == Blocks.SHULKER_BOX ||
-
-                state.getBlock() == Blocks.ANCIENT_DEBRIS;
     }
 }
